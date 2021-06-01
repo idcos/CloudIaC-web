bd8fc3d6c1d379e9580ea446be8d8c31140198d7
朱明浩 <zhuminghao@idcos.com>
2021-06-01 16:48:23 +0800

    销毁资源

--- app/containers/orgs/ct/detailPages/setting/components/destroy-resources.jsx
+++ app/containers/orgs/ct/detailPages/setting/components/destroy-resources.jsx
@@ -2,9 +2,21 @@ import React from "react";
 import { Button } from "antd";
 
 
-export default () => {
+export default (props) => {
+
+  const { detailInfo, onFinish } = props;
 
   return (
-    '销毁资源'
+    <div style={{ marginTop: 10 }}>
+      <Button
+        type='primary'
+        danger={true}
+      >
+        销毁资源
+      </Button>
+      <p className='tipText reset-styles'>
+        销毁资源将删除模板所有资源，包含作业、状态、变量、设置等等，请谨慎操作！
+      </p>
+    </div>
   );
 };
