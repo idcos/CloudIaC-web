0401d10cf4bedabfc706f65ff42ccfc03200b0ea
朱明浩 <zhuminghao@idcos.com>
2021-07-30 17:11:12 +0800

    [refactor] ahooks重构防抖

--- app/components/coder/ansi-coder-card/index.js
+++ app/components/coder/ansi-coder-card/index.js
@@ -1,23 +1,13 @@
-import React, { useState, useRef, useEffect } from "react";
-import {
-  Button,
-  Input,
-  Card,
-  Space
-} from "antd";
+import React, { useState, useRef } from "react";
+import { Button, Input, Card, Space } from "antd";
 import { VerticalAlignTopOutlined, VerticalAlignBottomOutlined, FullscreenExitOutlined, FullscreenOutlined } from "@ant-design/icons";
-
+import { default as AnsiUp } from 'ansi_up';
+import { useThrottleEffect } from 'ahooks';
 
 import { getNumLen } from 'utils/util';
-import { useThrottleHook } from 'utils/hooks';
-
 
 import styles from './styles.less';
 import SearchByKeyWord from './dom-event';
-
-import {
-  default as AnsiUp
-} from 'ansi_up';
 
 const ansi_up = new AnsiUp();
 const searchService = new SearchByKeyWord({ 
@@ -33,24 +23,28 @@ export default ({ value, cardTitleAfter }) => {
   const searchRef = useRef();
   const [ html, setHtml ] = useState('');
 
-  const throttleValue = useThrottleHook(value, 100);
-
-  useEffect(() => {
-    const maxLineIndexLen = getNumLen(throttleValue.length);
-    const lineIndexWidth = 6 + 8.5 * maxLineIndexLen;
-    const _html = throttleValue.map((line, index) => {
-      return `
-        <div class='ansi-line' style='padding-left: ${lineIndexWidth}px;'>
-          <span class='line-index' style='width: ${lineIndexWidth}px;'>${index + 1 }</span>
-          <pre class='line-text reset-styles'>${ansi_up.ansi_to_html(line)}</pre>
-        </div>
-      `;
-    }).join('');
-    setHtml(_html);
-    setTimeout(() => {
-      go('bottom');
-    });
-  }, [throttleValue]);
+  useThrottleEffect(
+    () => {
+      const maxLineIndexLen = getNumLen(value.length);
+      const lineIndexWidth = 6 + 8.5 * maxLineIndexLen;
+      const _html = value.map((line, index) => {
+        return `
+          <div class='ansi-line' style='padding-left: ${lineIndexWidth}px;'>
+            <span class='line-index' style='width: ${lineIndexWidth}px;'>${index + 1 }</span>
+            <pre class='line-text reset-styles'>${ansi_up.ansi_to_html(line)}</pre>
+          </div>
+        `;
+      }).join('');
+      setHtml(_html);
+      setTimeout(() => {
+        go('bottom');
+      });
+    },
+    [value],
+    {
+      wait: 100
+    }
+  );
 
   const go = (type) => {
     try {
