57e06c6e2f1c0467c040208c63508be06f6ea492
朱明浩 <zhuminghao@idcos.com>
2021-09-08 15:05:15 +0800

    [feat] 屏蔽功能开发

--- app/containers/compliance/policy-config/policy/detail-drawer/component/suppress/index.jsx
+++ app/containers/compliance/policy-config/policy/detail-drawer/component/suppress/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Card, Button, Space, Table, Alert, Drawer, Form } from 'antd';
+import { Card, Button, Space, Table, Modal } from 'antd';
+import { ExclamationCircleFilled } from '@ant-design/icons';
 import { useRequest } from 'ahooks';
 import { requestWrapper } from 'utils/request';
 import { useSearchFormAndTable } from 'utils/hooks';
@@ -63,10 +64,27 @@ export default ({ policyId }) => {
 
   const closeSuppressFormDrawer = () => setVisible(false);
 
+  const suppressPolicy = () => {
+    Modal.confirm({
+      width: 480,
+      title: '你确定要屏蔽此策略吗？',
+      content: '屏蔽此策略后，所有应用该策略的云模板和环境都将在检测过程中忽略此策略',
+      icon: <ExclamationCircleFilled />,
+      okText: '确认',
+    	cancelText: '取消',
+      onOk: () => {
+        console.log('onOk');
+      }
+    });
+  };
+
   return (
     <Card title='屏蔽内容'>
       <Space direction='vertical' size='middle' style={{ width: '100%' }}>
-        <Button type='primary' onClick={openSuppressFormDrawer}>按来源屏蔽</Button>
+        <Space>
+          <Button type='primary' onClick={suppressPolicy}>屏蔽此策略</Button>
+          <Button onClick={openSuppressFormDrawer}>按来源屏蔽</Button>
+        </Space>
         <Table
           columns={columns}
           loading={tableLoading}
