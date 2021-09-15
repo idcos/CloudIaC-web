f4f54eae619cef070a63198a64c5f8b39bc6b853
朱明浩 <zhuminghao@idcos.com>
2021-09-15 20:06:29 +0800

    [feat] sse接口最大暂停时间设置成1天

--- app/utils/hooks/useEventSource.jsx
+++ app/utils/hooks/useEventSource.jsx
@@ -6,7 +6,10 @@ export const useEventSource = () => {
   let eventSourceRef = useRef();
 
   const init = (listeners, { url, options }) => {
-    eventSourceRef.current = new EventSourcePolyfill(url, options);
+    eventSourceRef.current = new EventSourcePolyfill(url, {
+      heartbeatTimeout: 1000 * 60 * 60 * 24, // 长连接的最大等待时间默认值设置成1天
+      ...options
+    });
     _listeners.call(eventSourceRef.current, listeners);
   };
 
