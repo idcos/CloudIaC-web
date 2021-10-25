4a97ca9a7fefff7d340678ab9438e37573641c9f
朱明浩 <zhuminghao@idcos.com>
2021-10-25 22:02:51 +0800

    [feat] pipeline部署日志增加全屏功能和搜索功能

--- app/components/deploy-log-card/index.jsx
+++ app/components/deploy-log-card/index.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from "react";
-import { Card, Space, Button, Tag, Collapse } from "antd";
-import { CloseCircleFilled, CheckCircleFilled, SyncOutlined } from '@ant-design/icons';
+import React, { useState, useRef } from "react";
+import { Card, Space, Button, Tag, Collapse, Input } from "antd";
+import { CloseCircleFilled, CheckCircleFilled, SyncOutlined, FullscreenExitOutlined, FullscreenOutlined, SearchOutlined } from '@ant-design/icons';
 import { connect } from "react-redux";
-import { useRequest } from 'ahooks';
+import { useRequest, useFullscreen } from 'ahooks';
 import { requestWrapper } from 'utils/request';
 import getPermission from "utils/permission";
 import { TASK_STATUS, TASK_STATUS_COLOR, END_TASK_STATUS_LIST } from 'constants/types';
@@ -10,13 +10,23 @@ import envAPI from 'services/env';
 import taskAPI from 'services/task';
 import history from 'utils/history';
 import { timeUtils } from "utils/time";
+import SearchByKeyWord from 'components/coder/ansi-coder-card/dom-event';
 import DeployLog from './deploy-log';
 import styles from './styles.less';
 
 const { Panel } = Collapse;
+const searchService = new SearchByKeyWord({ 
+  searchWrapperSelect: '.ansi-coder-content',
+  excludeSearchClassNameList: [
+    'line-index'
+  ]
+});
 
 const DeployLogCard = ({ taskInfo, userInfo, reload }) => {
 
+  const searchRef = useRef();
+  const ref = useRef();
+  const [ isFullscreen, { toggleFull } ] = useFullscreen(ref);
   const { orgId, projectId, envId, id: taskId, startAt, endAt, type, status } = taskInfo || {};
   const { PROJECT_OPERATOR, PROJECT_APPROVER } = getPermission(userInfo);
   const [ activeKey, setActiveKey ] = useState();
@@ -102,87 +112,118 @@ const DeployLogCard = ({ taskInfo, userInfo, reload }) => {
   ); 
 
   return (
-    <Card
-      bodyStyle={{ background: 'rgba(36, 38, 35)', padding: 0 }}
-      title={
-        <div className={styles.card_title}>
-          <div className={styles.card_title_top}>
-            <span className={styles.title}>部署日志</span> 
-            <Tag className={styles.status} color={TASK_STATUS_COLOR[status]}>{TASK_STATUS[status]}</Tag>
+    <div ref={ref} className={styles.deploy_log_card_wrapper}>
+      <Card
+        className='deploy-log-card'
+        bodyStyle={{ background: 'rgba(36, 38, 35)', padding: 0 }}
+        title={
+          <div className='card-title'>
+            <div className='card-title-top'>
+              <span className='title'>部署日志</span> 
+              <Tag className='status' color={TASK_STATUS_COLOR[status]}>{TASK_STATUS[status]}</Tag>
+            </div>
+            <div className='card-title-bottom'>执行总耗时：{timeUtils.diff(endAt, startAt, '-')}</div>
           </div>
-          <div className={styles.card_title_bottom}>执行总耗时：{timeUtils.diff(endAt, startAt, '-')}</div>
-        </div>
-      }
-      extra={
-        <Space size={24}>
-          {
-            PROJECT_OPERATOR && (
-              <Space size={8}>
-                {
-                  taskInfo.status === 'approving' && (
-                    <>
-                      <Button 
-                        disabled={!PROJECT_APPROVER || approvedLoading}
-                        onClick={() => passOrRejecy('rejected')}
-                        loading={rejectedLoading} 
-                      >
-                        驳回
-                      </Button>
-                      <Button 
-                        disabled={!PROJECT_APPROVER || rejectedLoading}
-                        onClick={() => passOrRejecy('approved')} 
-                        loading={approvedLoading} 
-                        type='primary'
-                      >
-                        通过
-                      </Button>
-                    </>
-                  )
-                }
-                {
-                  type === 'plan' && status === 'complete' ? (
-                    <Button 
-                      type='primary'
-                      onClick={applyTask}
-                      loading={applyTaskLoading}
-                    >
-                      执行部署
-                    </Button>
-                  ) : null
-                }
-              </Space>
-            )
-          }
-        </Space>
-      }
-    >
-      <Collapse 
-        activeKey={activeKey} 
-        onChange={setActiveKey}
-        ghost={true} 
-        accordion={true}
-        className={styles.deploy_log_collapse}
-      >
-        {
-          taskSteps.map(({ name, id, startAt, endAt, status }) => (
-            <Panel 
-              header={
-                <Space>
-                  <span>{name || '-'}</span>
-                  {status === 'complete' && <CheckCircleFilled style={{ color: '#45BC13' }}/>}
-                  {status === 'failed' && <CloseCircleFilled style={{ color: '#F23C3C' }}/>}
-                  {status === 'running' && <SyncOutlined spin={true} style={{ color: '#ffffff' }}/>}
-                </Space>
-              } 
-              key={id}
-              extra={timeUtils.diff(endAt, startAt)}
-            >
-              <DeployLog taskInfo={taskInfo} stepId={id} stepStatus={status}/>
-            </Panel>
-          ))
         }
-      </Collapse>
-    </Card>
+        extra={
+          <Space size={24}>
+            {
+              PROJECT_OPERATOR && (
+                <Space size={8}>
+                  {
+                    taskInfo.status === 'approving' && (
+                      <>
+                        <Button 
+                          disabled={!PROJECT_APPROVER || approvedLoading}
+                          onClick={() => passOrRejecy('rejected')}
+                          loading={rejectedLoading} 
+                        >
+                          驳回
+                        </Button>
+                        <Button 
+                          disabled={!PROJECT_APPROVER || rejectedLoading}
+                          onClick={() => passOrRejecy('approved')} 
+                          loading={approvedLoading} 
+                          type='primary'
+                        >
+                          通过
+                        </Button>
+                      </>
+                    )
+                  }
+                  {
+                    type === 'plan' && status === 'complete' ? (
+                      <Button 
+                        type='primary'
+                        onClick={applyTask}
+                        loading={applyTaskLoading}
+                      >
+                        执行部署
+                      </Button>
+                    ) : null
+                  }
+                </Space>
+              )
+            }
+            <Input
+              prefix={<SearchOutlined style={{ color: '#242623' }} />}
+              ref={searchRef}
+              placeholder='搜索日志'
+              onPressEnter={(e) => {
+                searchService.search(e.target.value);
+                searchRef.current.focus();
+              }}
+              style={{ width: 240 }}
+            />
+            <span 
+              className='tool'
+              onClick={toggleFull} 
+            >
+              {
+                isFullscreen ? (
+                  <>
+                    <FullscreenExitOutlined className='tool-icon'/>
+                    <span className='tool-text'>退出全屏</span> 
+                  </>
+                ) : (
+                  <>
+                    <FullscreenOutlined className='tool-icon'/>
+                    <span className='tool-text'>全屏显示</span>
+                  </>
+                )
+              }
+            </span>
+          </Space>
+        }
+      >
+        <Collapse 
+          activeKey={activeKey} 
+          onChange={setActiveKey}
+          ghost={true} 
+          accordion={true}
+          className='deploy-log-collapse'
+        >
+          {
+            taskSteps.map(({ name, id, startAt, endAt, status }) => (
+              <Panel 
+                header={
+                  <Space>
+                    <span>{name || '-'}</span>
+                    {status === 'complete' && <CheckCircleFilled style={{ color: '#45BC13' }}/>}
+                    {status === 'failed' && <CloseCircleFilled style={{ color: '#F23C3C' }}/>}
+                    {status === 'running' && <SyncOutlined spin={true} style={{ color: '#ffffff' }}/>}
+                  </Space>
+                } 
+                key={id}
+                extra={timeUtils.diff(endAt, startAt)}
+              >
+                <DeployLog taskInfo={taskInfo} stepId={id} stepStatus={status}/>
+              </Panel>
+            ))
+          }
+        </Collapse>
+      </Card>
+    </div>
   );
 };
 
