c752f48043e2484bade6adf1dd7a107e81571b6e
朱明浩 <zhuminghao@idcos.com>
2021-11-16 10:26:50 +0800

    [feat] 任务步骤超时状态处理和失败状态一致

--- app/components/deploy-log-card/index.jsx
+++ app/components/deploy-log-card/index.jsx
@@ -87,6 +87,7 @@ const DeployLogCard = ({ taskInfo, userInfo, reload }) => {
       switch (item.status) {
         case 'complete':
         case 'failed':
+        case 'timeout':
         case 'running':
           activeKey = item.id;
           break;
@@ -248,12 +249,12 @@ const DeployLogCard = ({ taskInfo, userInfo, reload }) => {
           {
             taskSteps.map(({ name, id, startAt, type, endAt, status }) => (
               <Panel 
-                collapsible={!['complete', 'failed', 'running'].includes(status) && 'disabled'}
+                collapsible={!['complete', 'failed', 'timeout', 'running'].includes(status) && 'disabled'}
                 header={
                   <Space>
                     <span>{name || type || '-'}</span>
                     {status === 'complete' && <CheckCircleFilled style={{ color: '#45BC13' }}/>}
-                    {status === 'failed' && <CloseCircleFilled style={{ color: '#F23C3C' }}/>}
+                    {status === 'failed' || status === 'timeout' && <CloseCircleFilled style={{ color: '#F23C3C' }}/>}
                     {status === 'running' && <SyncOutlined spin={true} style={{ color: '#ffffff' }}/>}
                   </Space>
                 } 
