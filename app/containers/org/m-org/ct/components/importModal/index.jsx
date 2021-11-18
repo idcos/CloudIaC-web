6cdd2524a98c0d680d19e4959260f8d9a681d43b
fanwenbo <fanwenbo@idcos.com>
2021-11-18 21:09:33 +0800

    feat: 导入云模板增加报错回显

--- app/containers/org/m-org/ct/components/importModal/index.jsx
+++ app/containers/org/m-org/ct/components/importModal/index.jsx
@@ -4,6 +4,7 @@ import { VerticalAlignBottomOutlined } from '@ant-design/icons';
 import styles from './index.less';
 import tplAPI from 'services/tpl';
 import projectAPI from 'services/project';
+import { reduce } from 'lodash';
 
 const { Option } = Select;
 const infoType = {
@@ -11,17 +12,16 @@ const infoType = {
   updated: '覆盖',
   skipped: '跳过',
   copied: '创建副本'
-  //   renamed: '重命名',
-//   duplicate: '复制'
 };
 const infoErrorType = {
-  // renamed: '重命名',
-  duplicate: '复制'
+  renamed: '重命名',
+  duplicate: '中止'
 };
 const Index = ({ reload, toggleVisible, orgId }) => {
   const [ submitLoading, setSubmitLoading ] = useState(false);
-  const [ isImportSuccess, setIsImportSuccess ] = useState(false);
-  const [ importSuccessInfo, setImportSuccessInfo ] = useState({});
+  const [ importStatus, setImportStatus ] = useState('init');
+  const [ importInfo, setImportInfo ] = useState({});
+  const [ isImportSkipped, setIsImportSkipped ] = useState(false);
   const [ fileList, setFileList ] = useState([]);
   const [ type, setType ] = useState();
   const [ projectList, setProjectList ] = useState([]);
@@ -42,7 +42,16 @@ const Index = ({ reload, toggleVisible, orgId }) => {
       });
     }
   };
-
+  const computeCount = () => {
+    let count = 0;
+    let successTypeList = Object.keys(infoType);
+    Object.keys(importInfo).map((it, i) => {
+      if (successTypeList.includes(it)) {
+        count += (importInfo[it].templates || []).length;
+      }
+    });
+    return count;
+  };
   const onOk = async () => {
     if (fileList.length === 0) {
       return notification.error({ message: '请选择文件。' });
@@ -55,19 +64,25 @@ const Index = ({ reload, toggleVisible, orgId }) => {
     formData.set('file', fileList[0] && fileList[0]);
     formData.set('idDuplicate', type);
     formData.set('projects', selectProject);
-    
+
     const params = {
       file: formData,
       orgId
     };
+
     setSubmitLoading(true);
     const res = await tplAPI.importTemplates(params);
     setSubmitLoading(false);
+    
     if (res.code !== 200) {
+      setImportStatus('error');
+      if (res.result) {
+        setImportInfo(res.result || {});
+      }
       return notification.error({ message: res.message });
     } else {
-      setIsImportSuccess(true);
-      setImportSuccessInfo(res.result || {});
+      setImportStatus('success');
+      setImportInfo(res.result || {});
       reload();
     }
   };
@@ -82,28 +97,50 @@ const Index = ({ reload, toggleVisible, orgId }) => {
   };
 
   return <Modal
-    title={`${isImportSuccess ? '导入成功' : '导入云模板'}`}
+    title={`${importStatus === 'success' ? '导入成功' : importStatus === 'error' ? '导入失败' : '导入云模板'}`}
     visible={true}
     onCancel={toggleVisible}
     okButtonProps={{
       loading: submitLoading
     }}
     width={573}
-    onOk={isImportSuccess ? toggleVisible : onOk}
+    onOk={importStatus === 'init' ? onOk : toggleVisible}
+    footer={<>
+      {importStatus === 'init' ? <Space>
+        <Button onClick={toggleVisible}>取消</Button>
+        <Button type={'primary'} onClick={onOk}>确认</Button>
+      </Space> : <Button type={'primary'} onClick={toggleVisible}>知道了</Button>}
+    </>}
   >
-    {isImportSuccess ? (
+    {importStatus === 'success' && (
       <Space direction='vertical'>
-        {Object.keys(importSuccessInfo).map(it => {
-          return (importSuccessInfo[it].templates || []).map((dt) => {
+        <span style={{ fontWeight: 900 }}>成功导入云模板{computeCount('success')}条：</span>
+        {Object.keys(importInfo).map(it => {
+          return (importInfo[it].templates || []).map((dt) => {
             return (<span>
               {!!infoType[it] && <>
-                <span className={styles.resultTiele} style={{ color: it === 'create' ? '#088245' : '#000' }}>{infoType[it]}</span>: <span className={styles.resultText}>{dt.id}</span>
+                <span className={styles.resultTiele} style={{ color: it === 'create' ? '#088245' : '#000' }}> {infoType[it]}</span>: <span className={styles.resultText}>{dt.id}</span>
               </>}
             </span>);
           });
         })}
       </Space>
-    ) : (
+    )}
+    {importStatus === 'error' && (
+      <Space direction='vertical'>
+        <span style={{ fontWeight: 900 }}>UUID存在冲突，操作已中止，导入{computeCount('success')}条数据：</span>
+        {Object.keys(importInfo).map(it => {
+          return (importInfo[it].templates || []).map((dt) => {
+            return (<span>
+              {!!infoErrorType[it] && <>
+                <span className={styles.resultTiele} style={{ color: it === 'create' ? '#088245' : '#000' }}> {infoErrorType[it]}</span>: <span className={styles.resultText}>{dt.id}</span>
+              </>}
+            </span>);
+          });
+        })}
+      </Space>
+    )}
+    { importStatus === 'init' && (
       <Space direction='vertical'>
         <Space>
           <Upload 
