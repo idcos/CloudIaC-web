abc0bf5314b680a28e61d811c9dd053fbdf14de4
朱明浩 <zhuminghao@idcos.com>
2021-12-01 20:55:03 +0800

    [style] 云模版导入结果弹窗样式调整

--- app/containers/org/m-org/ct/components/importModal/index.jsx
+++ app/containers/org/m-org/ct/components/importModal/index.jsx
@@ -1,12 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { Button, Modal, Radio, Space, Upload, notification, Select, Tag } from 'antd';
+import React, { useState, useEffect, useMemo } from 'react';
+import { Button, Modal, Radio, Space, Upload, notification, Select } from 'antd';
 import { VerticalAlignBottomOutlined } from '@ant-design/icons';
 import { UploadtIcon } from 'components/iconfont';
-
+import classNames from 'classnames';
 import styles from './index.less';
 import tplAPI from 'services/tpl';
 import projectAPI from 'services/project';
-import { reduce } from 'lodash';
 
 const { Option } = Select;
 const infoType = {
@@ -119,6 +118,120 @@ const Index = ({ reload, toggleVisible, orgId }) => {
     fileList
   };
 
+  const modalContent = useMemo(() => {
+    switch (importStatus) {
+      case 'success':
+        return (
+          <Space direction='vertical' size={4}>
+            <div className={styles.resultHeader}>成功导入模板{computeCount('success')}条：</div>
+            {Object.keys(importInfo).map(it => {
+              return (importInfo[it].templates || []).map((dt) => {
+                return (<>
+                  {!!infoType[it] && <span>
+                    <span className={classNames(styles.resultTitle, { [styles.greenColor]: it === 'created' || it === 'copied' })}> {infoType[it]}【云模板】</span>: <span><span className={styles.resultText}>{dt.name}</span><span className={styles.resultText}>({dt.id})</span></span>
+                  </span>}
+                </>);
+              });
+            })}
+            {Object.keys(importInfo).map(it => {
+              return (importInfo[it].varGroups || []).map((dt) => {
+                return (<>
+                  {!!infoType[it] && <span>
+                    <span className={classNames(styles.resultTitle, { [styles.greenColor]: it === 'created' || it === 'copied' })}> {infoType[it]}【资源账号】</span>: <span><span className={styles.resultText}>{dt.name}</span><span className={styles.resultText}>({dt.id})</span></span>
+                  </span>}
+                </>);
+              });
+            })}
+            {Object.keys(importInfo).map(it => {
+              return (importInfo[it].vcs || []).map((dt) => {
+                return (<>
+                  {!!infoType[it] && <span>
+                    <span className={classNames(styles.resultTitle, { [styles.greenColor]: it === 'created' || it === 'copied' })}> {infoType[it]}【VCS】</span>: <span><span className={styles.resultText}>{dt.name}</span><span className={styles.resultText}>({dt.id})</span></span>
+                  </span>}
+                </>);
+              });
+            })}
+          </Space>
+        );
+      case 'error':
+        return (
+          <Space direction='vertical' size={4}>
+            <div className={styles.resultHeader}>UUID存在冲突，操作已中止，导入0条数据{hasColon ? '：' : '。'}</div>
+            {Object.keys(importInfo).map(it => {
+              return (importInfo[it].templates || []).map((dt) => {
+                return (<>
+                  {!!infoErrorType[it] && <span>
+                    <span className={classNames(styles.resultTitle, { [styles.greenColor]: it === 'created' || it === 'copied' })}> {infoErrorType[it]}【云模板】</span>: <span className={styles.resultText}>{dt.id}</span>
+                  </span>}
+                </>);
+              });
+            })}
+            {Object.keys(importInfo).map(it => {
+              return (importInfo[it].varGroups || []).map((dt) => {
+                return (<>
+                  {!!infoErrorType[it] && <span>
+                    <span className={classNames(styles.resultTitle, { [styles.greenColor]: it === 'created' || it === 'copied' })}> {infoErrorType[it]}【资源账号】</span>: <span><span className={styles.resultText}>{dt.name}</span><span className={styles.resultText}>({dt.id})</span></span>
+                  </span>}
+                </>);
+              });
+            })}
+            {Object.keys(importInfo).map(it => {
+              return (importInfo[it].vcs || []).map((dt) => {
+                return (<>
+                  {!!infoErrorType[it] && <span>
+                    <span className={classNames(styles.resultTitle, { [styles.greenColor]: it === 'created' || it === 'copied' })}> {infoErrorType[it]}【VCS】</span>: <span><span className={styles.resultText}>{dt.name}</span><span className={styles.resultText}>({dt.id})</span></span>
+                  </span>}
+                </>);
+              });
+            })}
+          </Space>
+        );
+      case 'init':
+        return (
+          <Space direction='vertical' size='middle'>
+            <Space>
+              <Upload 
+                {...props} 
+              >
+                <Button
+                  icon={<>{fileList[0] && fileList[0] ? <UploadtIcon/> : <VerticalAlignBottomOutlined/>}</>}
+                  style={{ borderTopLeftRadius: 4, borderBottomLeftRadius: 4 }}
+                >
+                  {fileList[0] && fileList[0] ? '重新选择' : '选择文件'}
+                </Button>
+              </Upload>
+              <span className={styles.radioText} style={{ marginLeft: 0, color: fileList[0] && fileList[0] ? '#00AB9D' : 'none' }}>{fileList[0] && fileList[0] ? fileList[0].name : '支持json文件'}</span> 
+            </Space>
+            <Space direction='vertical' size={6}>
+              <div className={styles.importHeader}><span>*</span> 导入时以UUID作为是否重复导入的依据，UUID重复时的操作方式：</div>
+              <Radio.Group onChange={(e) => setType(e.target.value)} value={type}>
+                <Space direction='vertical' size={4}>
+                  <Radio value={'update'}>覆盖 <span className={styles.radioText}>UUID重复时更新该条数据（包括云模板、VCS、资源帐号）</span></Radio> 
+                  <Radio value={'skip'}>跳过 <span className={styles.radioText}>UUID重复时跳过该条数据，继续导入其他数据</span></Radio> 
+                  <Radio value={'copy'}>创建副本 <span style={{ marginLeft: 0 }} className={styles.radioText}>UUID重复时，重新创建一条数据，如果名称也相同，给名称加上_copy后缀</span></Radio> 
+                  <Radio value={'abort'}>中止 <span className={styles.radioText}>UUID重复时中止导入操作，不做任何变更</span></Radio>
+                </Space>
+              </Radio.Group>
+            </Space>
+            <Space>
+              <span>关联项目：</span><Select 
+                getPopupContainer={triggerNode => triggerNode.parentNode}
+                placeholder='请选择关联项目'
+                mode={'multiple'}
+                showArrow={true}
+                style={{ width: 350 }}
+                onChange={e => setSelectProject(e)}
+              >
+                {(projectList || []).map(it => <Option value={it.id}>{it.name}</Option>)}
+              </Select>
+            </Space>
+          </Space>
+        );
+      default:
+        break;
+    }
+  });
+
   return <Modal
     title={`${importStatus === 'success' ? '导入成功' : importStatus === 'error' ? '导入失败' : '导入云模板'}`}
     visible={true}
@@ -135,110 +248,7 @@ const Index = ({ reload, toggleVisible, orgId }) => {
       </Space> : <Button type={'primary'} onClick={toggleVisible}>知道了</Button>}
     </>}
   >
-    {importStatus === 'success' && (
-      <Space direction='vertical'>
-        <span style={{ fontWeight: 900, fontSize: 16 }}>成功导入模板{computeCount('success')}条：</span>
-        {Object.keys(importInfo).map(it => {
-          return (importInfo[it].templates || []).map((dt) => {
-            return (<span>
-              {!!infoType[it] && <>
-                <span className={styles.resultTitle} style={{ color: it === 'created' || it === 'copied' ? '#088245' : '#000' }}> {infoType[it]}【云模板】</span>: <span><span className={styles.resultText}>{dt.name}</span><span className={styles.resultText}>({dt.id})</span></span>
-              </>}
-            </span>);
-          });
-        })}
-        {Object.keys(importInfo).map(it => {
-          return (importInfo[it].varGroups || []).map((dt) => {
-            return (<span>
-              {!!infoType[it] && <>
-                <span className={styles.resultTitle} style={{ color: it === 'created' || it === 'copied' ? '#088245' : '#000' }}> {infoType[it]}【资源账号】</span>: <span><span className={styles.resultText}>{dt.name}</span><span className={styles.resultText}>({dt.id})</span></span>
-              </>}
-            </span>);
-          });
-        })}
-        {Object.keys(importInfo).map(it => {
-          return (importInfo[it].vcs || []).map((dt) => {
-            return (<span>
-              {!!infoType[it] && <>
-                <span className={styles.resultTitle} style={{ color: it === 'created' || it === 'copied' ? '#088245' : '#000' }}> {infoType[it]}【VCS】</span>: <span><span className={styles.resultText}>{dt.name}</span><span className={styles.resultText}>({dt.id})</span></span>
-              </>}
-            </span>);
-          });
-        })}
-      </Space>
-    )}
-    {importStatus === 'error' && (
-      <Space direction='vertical'>
-        <span style={{ fontWeight: 900 }}>UUID存在冲突，操作已中止，导入0条数据{hasColon ? '：' : '。'}</span>
-        {Object.keys(importInfo).map(it => {
-          return (importInfo[it].templates || []).map((dt) => {
-            return (<span>
-              {!!infoErrorType[it] && <>
-                <span className={styles.resultTitle} style={{ color: it === 'created' || it === 'copied' ? '#088245' : '#000' }}> {infoErrorType[it]}【云模板】</span>: <span className={styles.resultText}>{dt.id}</span>
-              </>}
-            </span>);
-          });
-        })}
-        {Object.keys(importInfo).map(it => {
-          return (importInfo[it].varGroups || []).map((dt) => {
-            return (<span>
-              {!!infoErrorType[it] && <>
-                <span className={styles.resultTitle} style={{ color: it === 'created' || it === 'copied' ? '#088245' : '#000' }}> {infoErrorType[it]}【资源账号】</span>: <span><span className={styles.resultText}>{dt.name}</span><span className={styles.resultText}>({dt.id})</span></span>
-              </>}
-            </span>);
-          });
-        })}
-        {Object.keys(importInfo).map(it => {
-          return (importInfo[it].vcs || []).map((dt) => {
-            return (<span>
-              {!!infoErrorType[it] && <>
-                <span className={styles.resultTitle} style={{ color: it === 'created' || it === 'copied' ? '#088245' : '#000' }}> {infoErrorType[it]}【VCS】</span>: <span><span className={styles.resultText}>{dt.name}</span><span className={styles.resultText}>({dt.id})</span></span>
-              </>}
-            </span>);
-          });
-        })}
-      </Space>
-    )}
-    { importStatus === 'init' && (
-      <Space direction='vertical' size='middle'>
-        <Space>
-          <Upload 
-            {...props} 
-          >
-            <Button
-              icon={<>{fileList[0] && fileList[0] ? <UploadtIcon/> : <VerticalAlignBottomOutlined/>}</>}
-              style={{ borderTopLeftRadius: 4, borderBottomLeftRadius: 4 }}
-            >
-              {fileList[0] && fileList[0] ? '重新选择' : '选择文件'}
-            </Button>
-          </Upload>
-          <span className={styles.radioText} style={{ marginLeft: 0, color: fileList[0] && fileList[0] ? '#00AB9D' : 'none' }}>{fileList[0] && fileList[0] ? fileList[0].name : '支持json文件'}</span> 
-        </Space>
-        <Space direction='vertical' size={6}>
-          <div className={styles.importHeader}><span>*</span> 导入时以UUID作为是否重复导入的依据，UUID重复时的操作方式：</div>
-          <Radio.Group onChange={(e) => setType(e.target.value)} value={type}>
-            <Space direction='vertical' size={4}>
-              <Radio value={'update'}>覆盖 <span className={styles.radioText}>UUID重复时更新该条数据（包括云模板、VCS、资源帐号）</span></Radio> 
-              <Radio value={'skip'}>跳过 <span className={styles.radioText}>UUID重复时跳过该条数据，继续导入其他数据</span></Radio> 
-              <Radio value={'copy'}>创建副本 <span style={{ marginLeft: 0 }} className={styles.radioText}>UUID重复时，重新创建一条数据，如果名称也相同，给名称加上_copy后缀</span></Radio> 
-              <Radio value={'abort'}>中止 <span className={styles.radioText}>UUID重复时中止导入操作，不做任何变更</span></Radio>
-            </Space>
-          </Radio.Group>
-        </Space>
-        <Space>
-          <span>关联项目：</span><Select 
-            getPopupContainer={triggerNode => triggerNode.parentNode}
-            placeholder='请选择关联项目'
-            mode={'multiple'}
-            showArrow={true}
-            style={{ width: 350 }}
-            onChange={e => setSelectProject(e)}
-          >
-            {(projectList || []).map(it => <Option value={it.id}>{it.name}</Option>)}
-          </Select>
-        </Space>
-      </Space>)}
-
+    {modalContent}
   </Modal>;
 };
 export default Index;
