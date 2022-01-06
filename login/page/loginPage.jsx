7707a42a2e1b9fac73cede0c6dca14b3b2bbc23f
朱明浩 <zhuminghao@idcos.com>
2022-01-06 17:23:30 +0800

    [feat] 登陆页面调整

--- login/page/loginPage.jsx
+++ login/page/loginPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Form, Input, Button, notification } from 'antd';
+import { Form, Input, Button, notification, Row, Col } from 'antd';
 import queryString from 'query-string';
 
 import { authAPI } from "../services/auth";
@@ -70,53 +70,66 @@ export default () => {
   };
 
   return (
-    <div className={styles.login}>
+    <Row wrap={false} className={styles.login}>
+      <Col span={14} className='left'>
+        <div className='logo'>
+          <img src='/assets/logo/iac-logo-light.svg' alt='logo'/>
+        </div>
+        <div className='content'>
+          <div className='title'>
+            <div>CloudlaC</div>
+            <div>基础设施即代码平台</div>
+          </div>
+          <div className='describe'>引领云原生运维</div>
+        </div>
+        <div className='foot'>Copyright © 2021 杭州云霁科技有限公司</div>
+      </Col>
+      <Col span={10} className='right'>
+        <div className='loginFormWrapper'>
+          <div className='title'>登录</div>
+          <Form
+            {...layout}
+            name='basic'
+            className='loginForm'
+            requiredMark='optional'
+            onFinish={onFinish}
+          >
+            <div>
+              <Form.Item
+                className='format-form-item'
+                label={
+                  <>
+                    <span>邮箱</span>
+                  </>
+                }
+                name='email'
+                rules={[
+                  { required: true, message: '请输入邮箱地址' }, 
+                  { type: 'email', message: '邮箱格式有误' }
+                ]}
+              >
+                <Input placeholder='请输入邮箱地址' />
+              </Form.Item>
+            </div>
+          
 
-      <div className='loginFormWrapper'>
-        <div className='title'>CloudIaC基础设施即代码</div>
-        <Form
-          {...layout}
-          name='basic'
-          className='loginForm'
-          requiredMark='optional'
-          onFinish={onFinish}
-        >
-          <div>
             <Form.Item
               className='format-form-item'
-              label={
-                <>
-                  <span>邮箱</span>
-                </>
-              }
-              name='email'
-              rules={[
-                { required: true, message: '请输入邮箱地址' }, 
-                { type: 'email', message: '邮箱格式有误' }
-              ]}
+              label='密码'
+              name='password'
+              rules={[{ required: true, message: '请输入登录密码!' }]}
             >
-              <Input placeholder='请输入邮箱地址' />
+              <Input.Password placeholder='请输入登录密码' />
             </Form.Item>
-          </div>
-         
 
-          <Form.Item
-            className='format-form-item'
-            label='密码'
-            name='password'
-            rules={[{ required: true, message: '请输入登录密码!' }]}
-          >
-            <Input.Password placeholder='请输入登录密码' />
-          </Form.Item>
-
-          <Form.Item {...tailLayout} style={{ paddingTop: 8 }}>
-            <Button style={{ height: 36 }} block={true} type='primary' htmlType='submit'>
-              登录
-            </Button>
-          </Form.Item>
-        </Form>
-      </div>
-     
-    </div>
+            <Form.Item {...tailLayout} style={{ paddingTop: 8 }}>
+              <Button style={{ height: 36 }} block={true} type='primary' htmlType='submit'>
+                登录
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </Col>
+    </Row>
   );
 };
