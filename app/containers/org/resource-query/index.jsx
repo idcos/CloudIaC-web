263755053ca4fbc2edb4e5f76abceed696d87b8f
朱明浩 <zhuminghao@idcos.com>
2022-03-28 20:29:22 +0800

    Feat: 优化资源查询页面写法

--- app/containers/org/resource-query/index.jsx
+++ app/containers/org/resource-query/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Pagination, Checkbox, Input } from 'antd';
+import { Pagination, Checkbox, Input, Empty, Spin } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
 import { useRequest } from 'ahooks';
 import { requestWrapper } from 'utils/request';
@@ -7,75 +7,63 @@ import PageHeader from 'components/pageHeader';
 import Layout from 'components/common/layout';
 import classNames from 'classnames';
 import orgsAPI from 'services/orgs';
-import envAPI from 'services/env';
 import styles from './styles.less';
 import ResourceItem from './component/resource_item';
-
-const reqParams = {
-  pageSize: 10
-};
 
 export default ({ match }) => {
 
   const { orgId } = match.params || {};
+  const [ page, setPage ] = useState({ currentPage: 1, pageSize: 10 });
+  const [ searchParams, setSearchParams ] = useState({});
+  const [ searchCount, setSearchCount ] = useState(1);
 
   // 列表查询
   const {
     loading: tableLoading,
-    data: resourcesList = {},
-    run: getResourcesList
+    data: resources = {
+      list: [],
+      total: 0,
+      pageSize: 10
+    }
   } = useRequest(
-    (params) => requestWrapper(
-      orgsAPI.listResources.bind(null, { orgId, ...params })
+    () => requestWrapper(
+      orgsAPI.listResources.bind(null, { orgId, ...page, ...searchParams })
     ), {
       throttleInterval: 1000, // 节流
-      defaultParams: [reqParams],
-      // manual: true,
-      onSuccess: () => {
-        console.log(resourcesList);
-      }
+      refreshDeps: [searchCount]
     }
   );
 
   //获取环境和provider列表
   const {
-    data: providerAndEnvlist = {},
-    run: getProvidersAndEnvs
+    data: {
+      envs = [],
+      providers = []
+    } = {}
   } = useRequest(
-    (params) => requestWrapper(
+    () => requestWrapper(
       orgsAPI.filters.bind(null, { orgId })
     ), {
-      throttleInterval: 1000, // 节流
-      // manual: true，
       formatResult: (data) => {
-        let tempData = {};
-        tempData.envs = (data || {}).envs.map((val) => {
-          return { label: val.envName, value: val.envId };
-        });
-        tempData.providers = (data || {}).Providers.map((val) => {
-          return { label: val, value: val };
-        });
-        return tempData;
-      },
-      onSuccess: () => {
-        console.log(providerAndEnvlist);
+        const { envs, Providers } = data || {};
+        return {
+          envs: envs.map((val) => ({ label: val.envName, value: val.envId })),
+          providers: Providers.map((val) => ({ label: val, value: val }))
+        };
       }
     }
   );
 
+  const onParamsSearch = (params) => {
+    setSearchParams(preValue => ({ ...preValue, ...params }));
+    setPage(preValue => ({ ...preValue, currentPage: 1 }));
+    setSearchCount(preValue => preValue + 1);
+  };
 
-  const {
-    loading,
-    data,
-    run,
-    mutate
-  } = useRequest(
-    () => requestWrapper(
-      envAPI.getResources.bind(null, { envId: "env-c8acft3n6m8da397gnrg", orgId: "org-c8a738rn6m8fge3vg06g", projectId: "p-c8a73obn6m8fv9d3p1g0", resourceId: "r-c8ack1jn6m8da397gou0" })
-    ), {
-      // manual: true
-    }
-  );
+  const onPageSearch = (currentPage, pageSize) => {
+    setPage({ currentPage, pageSize });
+    setSearchCount(preValue => preValue + 1);
+  };
 
   return (
     <Layout
@@ -83,16 +71,13 @@ export default ({ match }) => {
         <PageHeader
           title={
             <div className={styles.search}>
-              <span style={{ fontSize: "20px" }}>资源查询</span>
+              <span style={{ fontSize: 20 }}>资源查询</span>
               <Input
                 allowClear={true}
-                style={{ width: "400px", marginLeft: "135px", height: "32px" }}
+                style={{ width: 400, marginLeft: 135, height: 32 }}
                 placeholder='请输入关键字搜索'
                 prefix={<SearchOutlined />}
-                onChange={(e) => {
-                  reqParams.q = e.target.value || '';
-                  getResourcesList(reqParams); 
-                }}
+                onPressEnter={(e) => onParamsSearch({ q: e.target.value })}
               />
             </div>}
           breadcrumb={true}
@@ -106,11 +91,8 @@ export default ({ match }) => {
             <Checkbox.Group 
               className={styles.checbox}
               style={{ width: '100%' }} 
-              options={providerAndEnvlist.envs}
-              onChange={(v) => {
-                reqParams.envIds = v.length > 0 ? v : undefined;
-                getResourcesList(reqParams); 
-              }}  
+              options={envs}
+              onChange={(v) => onParamsSearch({ envIds: v.length > 0 ? v : undefined })}  
             >
             </Checkbox.Group>
           </div>
@@ -119,32 +101,30 @@ export default ({ match }) => {
             <Checkbox.Group 
               className={styles.checbox}
               style={{ width: '100%' }} 
-              options={providerAndEnvlist.providers}
-              onChange={(v) => {
-                reqParams.providers = v.length > 0 ? v : undefined;
-                getResourcesList(reqParams); 
-              }}
+              options={providers}
+              onChange={(v) => onParamsSearch({ providers: v.length > 0 ? v : undefined })}  
             >
             </Checkbox.Group>
           </div>
         </div>
         <div className={styles.right}>
-          {resourcesList.list ? resourcesList.list.map((val) => {
-            return <ResourceItem {...val} />;
-          }) : null}
+          {tableLoading ? <Spin className='spinning' spinning={true} /> : (
+            resources.list.length ? (
+              resources.list.map((val) => {
+                return <ResourceItem {...val} />;
+              })
+            ) : <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
+          )}
           <div className={styles.pagination}>
-            { resourcesList.list ? 
-              <Pagination 
-                defaultCurrent={1} 
-                defaultPageSize={resourcesList.pageSize} 
-                total={resourcesList.total} 
-                onChange={(page, pageSize) => {
-                  reqParams.currentPage = page;
-                  reqParams.pageSize = pageSize;
-                  getResourcesList(reqParams); 
-                }}
-              /> : null
-            }
+            <Pagination 
+              size='default'
+              total={resources.total} 
+              hideOnSinglePage={true}
+              pageSize={page.pageSize}
+              current={page.currentPage}
+              onChange={onPageSearch}
+              showTotal={(total) => `共${total}条`}
+            />
           </div>
         </div>
       </div>
