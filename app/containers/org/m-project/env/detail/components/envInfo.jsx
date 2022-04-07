023d745cc589db1455c8e0b8e1cf9ba507c264da
fanwenboL <fanwenbo@idcos.com>
2022-04-07 17:36:21 +0800

    feat: 修改环境概览详情排序

--- app/containers/org/m-project/env/detail/components/envInfo.jsx
+++ app/containers/org/m-project/env/detail/components/envInfo.jsx
@@ -51,22 +51,27 @@ const EnvInfo = () => {
             contentStyle={{ color: '#57606A' }}
           >
             <Descriptions.Item span={3} label='当前费用'>{envInfo.monthCost}元</Descriptions.Item>
-            <Descriptions.Item span={3} label='存活时间'>{formatTTL(envInfo)}</Descriptions.Item>
-            <Descriptions.Item span={3} label='云模板'>{envInfo.templateName || '-'}</Descriptions.Item>
             <Descriptions.Item span={3} label='资源数'>{envInfo.resourceCount}</Descriptions.Item>
+
+            <Descriptions.Item span={3} label='存活时间'>{formatTTL(envInfo)}</Descriptions.Item>
             <Descriptions.Item span={3} label='更新时间'>{timeUtils.format(envInfo.updatedAt) || '-'}</Descriptions.Item>
+
+            <Descriptions.Item span={3} label='云模板'>{envInfo.templateName || '-'}</Descriptions.Item>
+            <Descriptions.Item span={3} label='分支/标签'>{envInfo.revision || '-'}</Descriptions.Item>
             <Descriptions.Item span={3} label='Commit_ID'><span onClick={() => {
               window.open(`${taskInfo.repoAddr.replace('.git', '')}/commit/${taskInfo.commitId}`);
             }} className={styles.linkToCommit}
             >{taskInfo.commitId && taskInfo.commitId.substring(0, 12) || '-'}</span></Descriptions.Item>
-            <Descriptions.Item span={3} label='分支/标签'>{envInfo.revision || '-'}</Descriptions.Item>
+
             <Descriptions.Item span={3} label='密钥'>{envInfo.keyName || '-'}</Descriptions.Item>
             <Descriptions.Item span={3} label='tfvars文件'>{envInfo.tfVarsFile || '-'}</Descriptions.Item>
             <Descriptions.Item span={3} label='playbook文件'>{envInfo.playbook || '-'}</Descriptions.Item>
+
             <Descriptions.Item span={3} label='部署通道'>{envInfo.runnerId || '-'}</Descriptions.Item>
             <Descriptions.Item span={3} label='target'>{envInfo.target || '-'}</Descriptions.Item>
             <Descriptions.Item span={3} label='推送到分支时重新部署'>{(envInfo.triggers || []).includes('commit') ? '是' : '否' }</Descriptions.Item>
             <Descriptions.Item span={3} label='PR/MR时执行PLAN'>{(envInfo.triggers || []).includes('prmr') ? '是' : '否' }</Descriptions.Item>
+           
             <Descriptions.Item span={3} label='创建人'>{envInfo.creator || '-'}</Descriptions.Item>
             <Descriptions.Item span={3} label='创建时间'>{timeUtils.format(envInfo.createdAt) || '-'}</Descriptions.Item>
           </Descriptions>
