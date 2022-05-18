ec444985f8f31ede72449b45cba74508d870b764
朱明浩 <zhuminghao@idcos.com>
2022-05-18 11:42:08 +0800

    Feat: 组织增加ldap邀请用户方式

---
+++ app/containers/org/m-org/setting/user-role/components/ldapModal.jsx
@@ -0,0 +1,140 @@
+import React, { useState } from 'react';
+import { Form, TreeSelect, Modal, Select, Radio } from 'antd';
+import { ORG_USER } from 'constants/types';
+import { t } from 'utils/i18n';
+
+const { Option } = Select;
+const FL = {
+  labelCol: { span: 4 },
+  wrapperCol: { span: 16 }
+};
+
+export default ({ visible, toggleVisible, operation, opt, curRecord, ORG_SET }) => {
+
+  const [ submitLoading, setSubmitLoading ] = useState(false);
+  
+  const [form] = Form.useForm();
+  
+  const onOk = async () => {
+    const formValues = await form.validateFields();
+    setSubmitLoading(true);
+    operation({
+      doWhat: opt,
+      payload: {
+        ...formValues,
+        id: curRecord && curRecord.id
+      }
+    }, (hasError) => {
+      setSubmitLoading(false);
+      !hasError && toggleVisible();
+    });
+  };
+
+  const treeData = [
+    {
+      title: 'Node1',
+      value: '0-0',
+      children: [
+        {
+          title: 'Child Node1',
+          value: '0-0-1'
+        },
+        {
+          title: 'Child Node2',
+          value: '0-0-2'
+        }
+      ]
+    },
+    {
+      title: 'Node2',
+      value: '0-1'
+    }
+  ];
+  
+  return (
+    <Modal
+      width={560}
+      title={t('define.org.user.action.ldapAdd')}
+      visible={visible}
+      onCancel={toggleVisible}
+      className='antd-modal-type-form'
+      okButtonProps={{
+        loading: submitLoading
+      }}
+      cancelButtonProps={{ 
+        className: 'ant-btn-tertiary' 
+      }}
+      onOk={onOk}
+    >
+      <Form
+        {...FL}
+        form={form}
+      >
+        <Form.Item
+          label={t('define.page.userSet.basic.field.type')}
+          name='type'
+          required={true}
+          initialValue='LDAP/OU'
+          treeData={treeData}
+        >
+          <Radio.Group>
+            <Radio value='LDAP/OU'>LDAP/OU</Radio>
+            <Radio value='LDAP/User'>LDAP/User</Radio>
+          </Radio.Group>
+        </Form.Item>
+        <Form.Item noStyle={true} shouldUpdate={true}>
+          {(form) => {
+            const { type } = form.getFieldsValue();
+            return type === 'LDAP/OU' ? (
+              <Form.Item
+                label='OU'
+                name='ou'
+                required={true}
+              >
+                <TreeSelect 
+                  getPopupContainer={triggerNode => triggerNode.parentNode}
+                  placeholder={t('define.form.select.placeholder')}
+                  treeData={treeData}
+                  treeDefaultExpandAll={true}
+                />
+              </Form.Item>
+            ) : (
+              <Form.Item
+                label='User'
+                name='user'
+                required={true}
+              >
+                <Select 
+                  getPopupContainer={triggerNode => triggerNode.parentNode}
+                  placeholder={t('define.form.select.placeholder')}
+                  options={[{ label: '1', value: '1' }]}
+                />
+              </Form.Item>
+            );
+          }}
+        </Form.Item>
+        <Form.Item
+          label={t('define.org.user.role')}
+          name='role'
+          rules={[
+            {
+              required: true,
+              message: t('define.form.select.placeholder')
+            }
+          ]}
+        >
+          <Select 
+            getPopupContainer={triggerNode => triggerNode.parentNode}
+            placeholder={t('define.form.select.placeholder')}
+          >
+            {ORG_SET ? (
+              Object.keys(ORG_USER.role).map(it => <Option value={it}>{ORG_USER.role[it]}</Option>)
+            ) : (
+              <Option value='member'>{t('org.role.member')}</Option>
+            )}
+          </Select>
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+};
--- app/containers/org/m-org/setting/user-role/index.jsx
+++ app/containers/org/m-org/setting/user-role/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Table, notification, Space, Divider, Popconfirm, Modal } from 'antd';
-import { InfoCircleFilled } from '@ant-design/icons';
+import { Button, Table, notification, Space, Dropdown, Popconfirm, Modal, Menu, Tabs } from 'antd';
+import { InfoCircleFilled, DownOutlined } from '@ant-design/icons';
 import moment from 'moment';
 import orgsAPI from 'services/orgs';
 import userAPI from 'services/user';
@@ -9,12 +9,15 @@ import EllipsisText from 'components/EllipsisText';
 import getPermission from "utils/permission";
 import { t } from 'utils/i18n';
 import OpModal from './components/memberModal';
+import LdapModal from './components/ldapModal';
 
 export default ({ userInfo, orgId }) => {
 
   const { ORG_SET } = getPermission(userInfo);
   const [ loading, setLoading ] = useState(false),
     [ visible, setVisible ] = useState(false),
+    [ tabKey, setTabKey ] = useState('user'),
+    [ ldapVisible, setLdapVisible ] = useState(false),
     [ isBatch, setIsBatch ] = useState(false),
     [ opt, setOpt ] = useState(null),
     [ curRecord, setCurRecord ] = useState(null),
@@ -68,9 +71,11 @@ export default ({ userInfo, orgId }) => {
       const method = {
         edit: (param) => orgsAPI.updateUser(param),
         add: (param) => orgsAPI.inviteUser(param),
+        ldapAdd: (param) => orgsAPI.inviteLdapUser(param),
         batchAdd: (param) => orgsAPI.batchInviteUser(param),
         resetUserPwd: ({ orgId, id }) => userAPI.resetUserPwd({ orgId, id }),
-        removeUser: ({ orgId, id }) => orgsAPI.removeUser({ orgId, id })
+        removeUser: ({ orgId, id }) => orgsAPI.removeUser({ orgId, id }),
+        removeLdapUser: ({ orgId, id }) => orgsAPI.removeLdapUser({ orgId, id })
       };
       const res = await method[doWhat]({
         orgId,
@@ -101,10 +106,37 @@ export default ({ userInfo, orgId }) => {
     setVisible(!visible);
   };
 
+  const toggleLdapVisible = () => {
+    if (visible) {
+      setCurRecord(null);
+      setOpt(null);
+    }
+    setLdapVisible(!ldapVisible);
+  };
+
   const remove = ({ id, name }) => {
     Modal.confirm({
       width: 480,
       title: `${t('define.org.user.action.remove.confirm.title.prefix')} ${name} ${t('define.org.user.action.remove.confirm.title.suffix')}`,
+      content: t('define.org.user.action.remove.confirm.content'),
+      icon: <InfoCircleFilled />,
+      okText: t('define.org.user.action.remove'),
+      okButtonProps: {
+        danger: true
+      },
+      cancelButtonProps: {
+        className: 'ant-btn-tertiary' 
+      },
+      onOk: () => {
+        return operation({ doWhat: 'removeLdapUser', payload: { id } });
+      }
+    });
+  };
+
+  const removeOU = ({ id, name }) => {
+    Modal.confirm({
+      width: 480,
+      title: `${t('define.org.user.action.remove.confirm.title.prefix')} ${name} ?`,
       content: t('define.org.user.action.remove.confirm.content'),
       icon: <InfoCircleFilled />,
       okText: t('define.org.user.action.remove'),
@@ -183,49 +215,146 @@ export default ({ userInfo, orgId }) => {
     }
   ];
 
+  const OUColumns = [
+    {
+      dataIndex: 'name',
+      title: 'OU',
+      width: 268,
+      ellipsis: true
+    },
+    {
+      dataIndex: 'role',
+      title: t('define.org.user.role'),
+      width: 160,
+      ellipsis: true,
+      render: (text) => ORG_USER.role[text]
+    },
+    {
+      dataIndex: 'createdAt',
+      title: t('define.org.user.createdAt'),
+      width: 212,
+      ellipsis: true,
+      render: (text) => moment(text).format('YYYY-MM-DD HH:mm:ss')
+    },
+    {
+      title: t('define.action'),
+      width: 180,
+      ellipsis: true,
+      fixed: 'right',
+      render: (_, record) => {
+        return (
+          <div className='common-table-btn-wrapper'>
+            <Button type='link' disabled={!ORG_SET} onClick={() => removeOU(record)}>{t('define.org.user.action.remove')}</Button>
+          </div>
+        );
+      }
+    }
+  ];
+
+
+
+  const invitation = (e) => {
+    switch (e.key) {
+    case 'invitation':
+      setOpt('add');
+      toggleVisible();
+      setIsBatch(false);
+      break;
+    case 'batch-invitation':
+      setOpt('add');
+      toggleVisible();
+      setIsBatch(true);
+      break;
+    case 'ldap-invitation':
+      setOpt('ldapAdd');
+      toggleLdapVisible();
+      break;
+    default:
+      break;
+    }
+  };
+
+  const menu = (
+    <Menu onClick={invitation}>
+      <Menu.Item key='invitation'>{t('define.org.user.action.add')}</Menu.Item>
+      <Menu.Item key='batch-invitation'>{t('define.org.user.action.batchAdd')}</Menu.Item>
+      <Menu.Item key='ldap-invitation'>{t('define.org.user.action.ldapAdd')}</Menu.Item>
+    </Menu>
+  );
+
   return <div>
     <div style={{ marginBottom: 20 }}>
-      <Button 
-        type='primary'
-        onClick={() => {
-          setOpt('add');
-          toggleVisible();
-          setIsBatch(false);
-        }}
-      >{t('define.org.user.action.add')}</Button>
-      <Button 
-        style={{ marginLeft: 8 }}
-        onClick={() => {
-          setOpt('add');
-          toggleVisible();
-          setIsBatch(true);
-        }}
-      >{t('define.org.user.action.batchAdd')}</Button>
+      <Dropdown overlay={menu}>
+        <Button>
+          <Space>
+            {t('define.org.user.action.addWrapper')}
+            <DownOutlined />
+          </Space>
+        </Button>
+      </Dropdown>
     </div>
-    <Table
-      columns={columns}
-      dataSource={resultMap.list}
-      loading={loading}
-      scroll={{ x: 'min-content' }}
-      pagination={{
-        current: query.pageNo,
-        pageSize: query.pageSize,
-        total: resultMap.total,
-        showSizeChanger: true,
-        showQuickJumper: true,
-        showTotal: (total) => t('define.pagination.showTotal', { values: { total } }),
-        onChange: (page, pageSize) => {
-          changeQuery({
-            pageNo: page,
-            pageSize
-          });
-        }
-      }}
-    />
+    <Tabs activeKey={tabKey} onChange={setTabKey}>
+      <Tabs.TabPane tab={t('define.user')} key='user'>
+        <Table
+          columns={columns}
+          dataSource={resultMap.list}
+          loading={loading}
+          scroll={{ x: 'min-content' }}
+          pagination={{
+            current: query.pageNo,
+            pageSize: query.pageSize,
+            total: resultMap.total,
+            showSizeChanger: true,
+            showQuickJumper: true,
+            showTotal: (total) => t('define.pagination.showTotal', { values: { total } }),
+            onChange: (page, pageSize) => {
+              changeQuery({
+                pageNo: page,
+                pageSize
+              });
+            }
+          }}
+        />
+      </Tabs.TabPane>
+      <Tabs.TabPane tab='LDAP/OU' key='ou'>
+        <Table
+          columns={OUColumns}
+          dataSource={resultMap.list}
+          loading={loading}
+          scroll={{ x: 'min-content' }}
+          pagination={{
+            current: query.pageNo,
+            pageSize: query.pageSize,
+            total: resultMap.total,
+            showSizeChanger: true,
+            showQuickJumper: true,
+            showTotal: (total) => t('define.pagination.showTotal', { values: { total } }),
+            onChange: (page, pageSize) => {
+              changeQuery({
+                pageNo: page,
+                pageSize
+              });
+            }
+          }}
+        />
+      </Tabs.TabPane>
+    </Tabs>
+   
     {
       visible && <OpModal
         visible={visible}
         toggleVisible={toggleVisible}
+        opt={opt}
+        curRecord={curRecord}
+        operation={operation}
+        isBatch={isBatch}
+        ORG_SET={ORG_SET}
+      />
+    }
+    {
+      ldapVisible && <LdapModal
+        visible={ldapVisible}
+        toggleVisible={toggleLdapVisible}
         opt={opt}
         curRecord={curRecord}
         operation={operation}
