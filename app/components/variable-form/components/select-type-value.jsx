812a4c4e2cf4ea767fb5c2a0cb8d1fd0306565d5
fanwenbo <fanwenbo@idcos.com>
2022-07-08 10:44:05 +0800

    fix: CLOUDIAC-324 选择型变量添加value后不能删除

--- app/components/variable-form/components/select-type-value.jsx
+++ app/components/variable-form/components/select-type-value.jsx
@@ -3,6 +3,7 @@ import { Select, Input, Divider, Button, Space } from 'antd';
 import styled from 'styled-components';
 import isEmpty from 'lodash/isEmpty';
 import { t } from 'utils/i18n';
+import EllipsisText from '../../EllipsisText';
 
 const OptionWrapper = styled.div`
   display: flex;
@@ -19,17 +20,17 @@ const SelectTypeValue = ({
   form,
   ...props
 }) => {
-  const [options, setOptions] = useState([]);
-  const [inputValue, setInputValue] = useState();
+  const [ options, setOptions ] = useState([]);
+  const [ inputValue, setInputValue ] = useState();
 
   useEffect(() => {
     if (!isEmpty(inputOptions)) {
-      setOptions(inputOptions)
+      setOptions(inputOptions);
     }
   }, [inputOptions]);
 
   const addOption = () => {
-    const newOptions = [...options, inputValue];
+    const newOptions = [ ...options, inputValue ];
     setOptions(newOptions);
     setInputValue();
     form.setFieldsValue({ options: newOptions });
@@ -82,7 +83,7 @@ const SelectTypeValue = ({
         options.map(item => (
           <Select.Option key={item} value={item}>
             <OptionWrapper>
-              <span>{item}</span>
+              <EllipsisText style={{ width: 142 }}>{item}</EllipsisText>
               {isSameScope && <Button type='link' style={{ padding: 0 }} onClick={(e) => delOption(e, item)}>{t('define.action.delete')}</Button>}
             </OptionWrapper>
           </Select.Option>
