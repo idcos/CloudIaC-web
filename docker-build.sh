212bb467e4712ed850b166111ad05a59f8fbcfbb
Rogersjgls233233 <874088962@qq.com>
2022-11-03 11:43:48 +0800

    update: 1.2.4

--- docker-build.sh
+++ docker-build.sh
@@ -3,6 +3,6 @@ npm run build
 cd build/ && tar cpf ../conf/iac-web.tar ./*
 cd ../conf
 pwd
-docker build -t cloudiac/iac-web:1.2.0 . --platform linux/amd64
-docker push cloudiac/iac-web:1.2.0
+docker build -t cloudiac/iac-web:1.2.4 . --platform linux/amd64
+docker push cloudiac/iac-web:1.2.4
 rm ./iac-web.tar
