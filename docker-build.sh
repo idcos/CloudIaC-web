02ff809c73064d1bfb21cfc377121d7a7c808139
Rogersjgls233233 <874088962@qq.com>
2023-02-25 14:24:38 +0800

    update: docker sh v1.3.5

--- docker-build.sh
+++ docker-build.sh
@@ -3,8 +3,8 @@ npm run build
 cd build/ && tar cpf ../conf/iac-web.tar ./*
 cd ../conf
 pwd
-docker build -t cloudiac/iac-web:v1.3.3 . --platform linux/amd64
-docker push cloudiac/iac-web:v1.3.3
-docker tag cloudiac/iac-web:v1.3.3 cloudiac/iac-web:latest
+docker build -t cloudiac/iac-web:v1.3.5 . --platform linux/amd64
+docker push cloudiac/iac-web:v1.3.5
+docker tag cloudiac/iac-web:v1.3.5 cloudiac/iac-web:latest
 docker push cloudiac/iac-web:latest
 rm ./iac-web.tar
