019f754838b305baa5d2802641d97b809d102254
Rogersjgls233233 <874088962@qq.com>
2023-05-08 17:09:48 +0800

    feat: 活跃资源费用列表改版

--- app/containers/project/m-project/env/detail/components/cost-report/index.js
+++ app/containers/project/m-project/env/detail/components/cost-report/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { Card, Row, Col, Space, Spin, Table } from 'antd';
+import { LinkOutlined } from '@ant-design/icons';
 import { useRequest } from 'ahooks';
 import { requestWrapper } from 'utils/request';
 import envAPI from 'services/env';
@@ -67,25 +68,96 @@ const CostReport = ({ orgId, projectId, envId }) => {
     },
     {
       dataIndex: 'instanceSpec',
-      title: t('env.resource.instanceSpec'),
-      width: 150,
+      title: t('define.resource.specification'),
+      width: 120,
       ellipsis: true,
       render: text => (text && DISK_TYPE_ENUM[text]) || text || '-',
     },
     {
       dataIndex: 'subscriptionType',
-      title: t('env.resource.subscriptionType'),
+      title: t('define.resource.subscriptionType'),
       width: 80,
       ellipsis: true,
       render: text => (text && SUBSCRIPTION_TYPE_ENUM[text]) || text || '-',
     },
-
+    {
+      dataIndex: 'region',
+      title: t('define.resource.region'),
+      width: 80,
+      ellipsis: true,
+      render: text => text || '-',
+    },
     {
       dataIndex: 'instanceId',
       title: t('define.resource.instanceID'),
       width: 150,
       ellipsis: true,
-      render: text => text || '-',
+      render: (text, record) => {
+        switch (record.resType) {
+          case 'alicloud_instance':
+            return (
+              <span>
+                {!!record.instanceId && (
+                  <a
+                    href={`https://ecs.console.aliyun.com/server/${record.instanceId}/detail?regionId=${record.region}`}
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    <LinkOutlined style={{ marginRight: '5px' }} />
+                  </a>
+                )}
+                {text || '-'}
+              </span>
+            );
+          case 'alicloud_ecs_disk':
+            return (
+              <span>
+                {!!record.instanceId && (
+                  <a
+                    href={`https://ecs.console.aliyun.com/diskdetail/${record.instanceId}/detail?regionId=${record.region}`}
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    <LinkOutlined style={{ marginRight: '5px' }} />
+                  </a>
+                )}
+                {text || '-'}
+              </span>
+            );
+          case 'alicloud_slb_load_balancer':
+            return (
+              <span>
+                {!!record.instanceId && (
+                  <a
+                    href={`https://slb.console.aliyun.com/slb/${record.region}/slbs/${record.instanceId}`}
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    <LinkOutlined style={{ marginRight: '5px' }} />
+                  </a>
+                )}
+                {text || '-'}
+              </span>
+            );
+          case 'alicloud_eip_address':
+            return (
+              <span>
+                {!!record.instanceId && (
+                  <a
+                    href={`https://vpc.console.aliyun.com/eip/${record.region}/eips/${record.instanceId}`}
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    <LinkOutlined style={{ marginRight: '5px' }} />
+                  </a>
+                )}
+                {text || '-'}
+              </span>
+            );
+          default:
+            return <span>{text || '-'}</span>;
+        }
+      },
     },
     {
       dataIndex: 'curMonthCost',
